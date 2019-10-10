b22e36bfc47f2895aaedaf41794c2e7129eadb6b
chaumaibaole <chaumaibaole@192.168.1.10>
2019-10-09 17:57:51 -0700

    Second Version

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -6,14 +6,14 @@
 //    StudentID: 1215667284
 //      Lecture: MONDAY: 9:40AM - 10:30AM
 //  Description: 1. Understand how to create the local repository folder in git or github
-//			 2. Commit the project to git and github
-//			 3. Understand how to use git commit and commit change
-//			 4. Understand how to use push in git
-//			 5. Able to change file and commit the change to the git and github
-//			 6. Able to create a string expresses all of the operators and calculations
-//			    in the class
-//			 7. Perform addition and subtraction
-//			 8. Print the result perform all of the calculations
+//		 2. Commit the project to git and github
+//		 3. Understand how to use git commit and commit change
+//		 4. Understand how to use push in git
+//		 5. Able to change file and commit the change to the git and github
+//		 6. Able to create a string expresses all of the operators and calculations
+//		    in the class
+//		 7. Perform addition and subtraction
+//		 8. Print the result perform all of the calculations
 //     Filename: AddingMachine.java
 ////////////////////////////////////////////////////////////////////////////////////////////
 ///*****************************************************************************************
@@ -25,7 +25,7 @@ public class AddingMachine {			//class AddingMachine
 
 	private int total;			//total variable
 	
-	private String make;				//create a string in order to get get the right operator
+	private String make;			//create a string in order to get get the right operator
 	
 	public AddingMachine () {
 		total = 0;  			// not needed - included for clarity
