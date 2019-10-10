05e815d31fdbbe81719edc3bdbc8fdbfb359717f
chaumaibaole <chaumaibaole@192.168.1.10>
2019-10-09 18:40:12 -0700

    Second Version

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -6,14 +6,14 @@
 //    StudentID: 1215667284
 //      Lecture: MONDAY: 9:40AM - 10:30AM
 //  Description: 1. Understand how to create the local repository folder in git or github
-//		 2. Commit the project to git and github
-//		 3. Understand how to use git commit and commit change
-//		 4. Understand how to use push in git
-//		 5. Able to change file and commit the change to the git and github
-//		 6. Able to create a string expresses all of the operators and calculations
-//		    in the class
-//		 7. Perform addition and subtraction
-//		 8. Print the result perform all of the calculations
+//		 		 2. Commit the project to git and github
+//		 		 3. Understand how to use git commit and commit change
+//		 		 4. Understand how to use push in git
+//				 5. Able to change file and commit the change to the git and github
+//				 6. Able to create a string expresses all of the operators and calculations
+//				    in the class
+//				 7. Perform addition and subtraction
+//				 8. Print the result perform all of the calculations
 //     Filename: AddingMachine.java
 ////////////////////////////////////////////////////////////////////////////////////////////
 ///*****************************************************************************************
@@ -21,36 +21,36 @@
 
 package cse360assign2;
 
-public class AddingMachine {			//class AddingMachine
+public class AddingMachine {		//class AddingMachine
 
-	private int total;			//total variable
+	private int total;				//total variable
 	
 	private String make;			//create a string in order to get get the right operator
 	
 	public AddingMachine () {
-		total = 0;  			// not needed - included for clarity
-		make = "" + total;		//start from the first value of total variable
+		total = 0;  				// not needed - included for clarity
+		make = "" + total;			//start from the first value of total variable
 	}
 	
 	public int getTotal () {		//get the value of the total variable
-		return total;			//return total variable
+		return total;				//return total variable
 	}
 	
 	public void add (int value) {		//method for addition
-		total = total + value;		//add the parameter to the total variable
+		total = total + value;			//add the parameter to the total variable
 		make = make + " + " + value;	//create a string expresses for this event
 	}	
 	
 	public void subtract (int value) {	//method for subtraction
-		total = total - value;		//subtract the parameter from the total variable
+		total = total - value;			//subtract the parameter from the total variable
 		make = make + " - " + value;	//create a string expresses for this event
 	}
 		
 	public String toString () {		//string value of all the events
-		return make;			//return make string after all of the events
+		return make;				//return make string after all of the events
 	}
 
 	public void clear() {			//clear the current value of string make
-		make = "\0";			//string back to first initial
+		make = "\0";				//string back to first initial
 	}
 }
